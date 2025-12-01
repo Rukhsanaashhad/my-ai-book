fc9aa5dfc78ff44ee8176a23f66d9db61aad6881
Rukhsanaashhad <maxtraders712@gmail.com>
2025-12-01 20:10:08 +0500

    Deploy website - based on f02f2e653bd5209996f81d08e230cbd9f06919f6

--- README.md
+++
@@ -1,2 +0,0 @@
-# my-ai-book
-AI-Driven Development Guide - A complete book on AI tools, prompt engineering, and development workflows with practical examples.
---
+++ assets/js/36994c47.32f22838.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmy_book=globalThis.webpackChunkmy_book||[]).push([[858],{5516:o=>{o.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
